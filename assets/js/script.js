fac209540331dfdd993f1223644fbfefd899719b
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-15 13:08:54 -0500

    fixed syntax and location of localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var currentDay = document.querySelector("#currentDay")
 var today = moment().format("MMMM-D-YYYY")
 currentDay.append(today);
-var saveBtn = document.querySelector(".saveBtn")
+var saveBtn = $(".saveBtn")
+//var saveBtn = document.querySelector(".saveBtn")
 var update = setInterval(updateHour, 3600000)
 
 $(document).ready(function() {
@@ -11,16 +12,6 @@ $(saveBtn).on("click", function(){
     var textBox = $(this).siblings(".description").val()
 
     localStorage.setItem(time, textBox);
-    $("hour-9.description").val(localStorage.getItem("hour-9"))
-    $("hour-10.description").val(localStorage.getItem("hour-10"))
-    $("hour-11.description").val(localStorage.getItem("hour-11"))
-    $("hour-12.description").val(localStorage.getItem("hour-12"))
-    $("hour-13.description").val(localStorage.getItem("hour-13"))
-    $("hour-14.description").val(localStorage.getItem("hour-14"))
-    $("hour-15.description").val(localStorage.getItem("hour-15"))
-    $("hour-16.description").val(localStorage.getItem("hour-16"))
-    $("hour-17.description").val(localStorage.getItem("hour-"))
-
 })})
 
 function updateHour() {
@@ -39,5 +30,15 @@ function updateHour() {
             $(this).addClass("future")
         } 
 })}
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
 updateHour();
