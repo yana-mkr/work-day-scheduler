48f01c1561ddea4f948e41c114089291389e0187
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-04-15 13:19:32 -0500

    changed date format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
-var currentDay = document.querySelector("#currentDay")
-var today = moment().format("MMMM-D-YYYY")
+var currentDay =$("#currentDay")
+//var currentDay = document.querySelector("#currentDay")
+var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+// wasn't sure how to make line 3 in jquery syntax
 currentDay.append(today);
 var saveBtn = $(".saveBtn")
 //var saveBtn = document.querySelector(".saveBtn")
 var update = setInterval(updateHour, 3600000)
+// update every hour in milliseconds
 
 $(document).ready(function() {
+//jQuery detects this state of readiness
 
 $(saveBtn).on("click", function(){
     var time = $(this).parent().attr("id")
+    // pulling the parent of my hour-9 so that no loop is needed
     var textBox = $(this).siblings(".description").val()
+    // value fro mthe textBox, it's the sibling of the "time" var as well
 
     localStorage.setItem(time, textBox);
+    // set key, value
 })})
 
 function updateHour() {
 
    var current = moment().hour();
+   //console.log(current)
     $(".time-block").each(function() {
        var hour = parseInt($(this).attr("id").split("-")[1]);
         if (hour < current) {
@@ -28,6 +36,7 @@ function updateHour() {
             $(this).removeClass("past")
             $(this).removeClass("present")
             $(this).addClass("future")
+            //console.log(hour)
         } 
 })}
 
@@ -40,5 +49,6 @@ $("#hour-14 .description").val(localStorage.getItem("hour-14"))
 $("#hour-15 .description").val(localStorage.getItem("hour-15"))
 $("#hour-16 .description").val(localStorage.getItem("hour-16"))
 $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+// not sure if I could have made a loop for this
 
 updateHour();
